c7a676c1181b604dc8909ef134522a93b67fd57b
RubenGhukasyan <rubengukasyan15@gmail.com>
2025-07-13 18:01:04 +0400

    new Branch

--- src/Main.java
+++ src/Main.java
@@ -158,5 +158,6 @@ public class Main {
         mineSweeper game=new mineSweeper();
         System.out.println("input\nfl x y \nto put flag on (x,y) or \no x y\nto open x y");
         game.play();
+        System.out.println("new Branch");
     }
 }
